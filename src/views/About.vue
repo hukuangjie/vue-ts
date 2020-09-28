331438f43b88264cce5cc68de625cb4ac17df76c
胡旷杰 <754432607@qq.com>
2020-09-28 23:38:36 +0800

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ firstName }}</p>
+    <p>{{ lastName }}</p>
+    <p>{{ fullName }}</p>
+    <button @click="modifyFullname">修改fullname</button>
   </div>
 </template>
+<script lang="ts">
+import { Component, Watch, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class About extends Vue {
+  // 初始数据可以直接声明为实例的 property
+  firstName = "zhang";
+  lastName = "san";
+
+  // 组件方法也可以直接声明为实例的方法
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+  set fullName(val) {
+    const arr = val.split(" ");
+    this.firstName = arr[0];
+    this.lastName = arr[1];
+  }
+  modifyFullname() {
+    this.fullName = "li si";
+  }
+
+  @Watch("firstName")
+  onFirstNameChange(oldVal: string, newVal: string) {
+    console.log(oldVal, newVal);
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-09-28 22:11:05
+ * @LastEditTime: 2020-09-28 23:24:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \sxh\src\views\Home.vue
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component({
+  // components: {
+  //   HelloWorld,
+  // },
+})
+export default class Home extends Vue {}
 </script>
